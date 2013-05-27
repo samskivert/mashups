3360cdcdd50236f6ea7f8b880043af5035aeb561
Michael Bayne <mdb@samskivert.com>
2013-05-27 14:02:17 -0700

    An SBT build.

---
+++ gridpoker/project/Build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import sbt.Keys._
+
+object GameBuild extends samskivert.MavenBuild {
+
+  override val globalSettings = Seq(
+    crossPaths    := false,
+    javacOptions  ++= Seq("-Xlint", "-Xlint:-serial", "-source", "1.6", "-target", "1.6"),
+    javaOptions   ++= Seq("-ea"),
+    fork in Compile := true,
+    autoScalaLibrary in Compile := false, // no scala-library dependency
+    libraryDependencies ++= Seq(
+      "com.novocode" % "junit-interface" % "0.7" % "test->default" // make JUnit tests work
+    )
+  )
+
+  override def moduleSettings (name :String, pom :pomutil.POM) = name match {
+    case "java" => LWJGLPlugin.lwjglSettings ++ seq(
+      LWJGLPlugin.lwjgl.version := pom.getAttr("lwjgl.version").get
+    ) ++ spray.revolver.RevolverPlugin.Revolver.settings
+    case _ => Nil
+  }
+
+  override def profiles = Seq("java")
+}
