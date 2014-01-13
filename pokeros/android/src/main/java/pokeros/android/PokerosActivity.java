9eb2c1339742931b1277d064e1fa5aec87584ad4
Michael Bayne <mdb@samskivert.com>
2014-01-13 08:59:09 -0800

    Various "get things working" on Android.

--- pokeros/android/src/main/java/pokeros/android/PokerosActivity.java
+++ pokeros/android/src/main/java/pokeros/android/PokerosActivity.java
@@ -1,6 +1,14 @@
 package pokeros.android;
 
+import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+import playn.android.AndroidAssets;
 import playn.android.GameActivity;
+import playn.core.Font;
 import playn.core.PlayN;
 
 import pokeros.core.Pokeros;
@@ -9,6 +17,31 @@ public class PokerosActivity extends GameActivity {
 
   @Override
   public void main(){
-    PlayN.run(new Pokeros());
+    // register our fonts
+    platform().graphics().registerFont("fonts/copperplate.ttf", "Copperplate", Font.Style.PLAIN);
+    platform().graphics().registerFont("fonts/copperplate.ttf", "Copperplate", Font.Style.BOLD);
+
+    // use a lower-memory format for JPGs (which tend to be large and memory sucking)
+    platform().assets().setBitmapOptionsAdjuster(new AndroidAssets.BitmapOptionsAdjuster() {
+      @Override public void adjustOptions (String path, AndroidAssets.BitmapOptions options) {
+        // use a 16-bit per pixel format for JPGs; looks decent, saves memory
+        if (path.endsWith(".jpg")) options.inPreferredConfig = Bitmap.Config.RGB_565;
+      }
+    });
+
+    PlayN.run(new Pokeros(0.5f));
+  }
+
+  @Override protected float scaleFactor () {
+    DisplayMetrics dm = getResources().getDisplayMetrics();
+    return (dm.densityDpi >= DisplayMetrics.DENSITY_MEDIUM) ? 2 : 1;
+  }
+
+  @Override protected boolean usePortraitOrientation () { return true; }
+
+  @Override protected String logIdent () { return "pokeros"; }
+
+  @Override protected int makeWindowFlags () { // we want the status bar to remain visible
+    return super.makeWindowFlags() & ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
   }
 }
