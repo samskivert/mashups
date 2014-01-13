26091f42c66a9bef866960a27deba48652f6a967
Michael Bayne <mdb@samskivert.com>
2014-01-13 12:24:19 -0800

    Added device screen size emulation twiddling.

--- pokeros/java/src/main/java/pokeros/java/PokerosJava.java
+++ pokeros/java/src/main/java/pokeros/java/PokerosJava.java
@@ -11,14 +11,40 @@ import pokeros.core.Pokeros;
 
 public class PokerosJava {
 
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     JavaPlatform.Config config = new JavaPlatform.Config();
-    config.width = 320;
-    config.height = 480;
-    config.scaleFactor = 2;
+    if (args.length == 0 || !configSize(config, args[0])) {
+      config.width = 640/2;
+      config.height = 960/2;
+      config.scaleFactor = 2;
+    }
     config.emulateTouch = true;
-    // use config to customize the Java platform, if needed
     JavaPlatform.register(config);
     PlayN.run(new Pokeros(0.5f));
+  }
+
+  protected static boolean configSize (JavaPlatform.Config config, String device) {
+    if ("ipad".equals(device)) {
+      config.width = 768/2;
+      config.height = 1024/2;
+      config.scaleFactor = 2;
+    } else if ("i5".equals(device)) {
+      config.width = 640/2;
+      config.height = 1136/2;
+      config.scaleFactor = 2;
+    } else if ("droid".equals(device)) {
+      config.width = (int)(480/1.5f);
+      config.height = (int)(800/1.5f);
+      config.scaleFactor = 1.5f;
+    } else if ("n7".equals(device)) {
+      config.width = 1200/2;
+      config.height = 1920/2;
+      config.scaleFactor = 2;
+    } else if ("n10".equals(device)) {
+      config.width = 1600/2;
+      config.height = 2560/2;
+      config.scaleFactor = 2;
+    } else return false;
+    return true;
   }
 }
